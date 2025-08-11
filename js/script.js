254a8215cf769909baf1db0990eebb782649a469
MarianeMarinho <marianemarinho498@gmail.com>
2025-08-11 19:41:53 -0300

    feat: adding score logic and adjusting the gameboard

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 const mario = document.querySelector(".mario");
 const pipes = document.querySelector(".pipes");
+const clouds = document.querySelector(".clouds");
 
 const jump = () => {
-  mario.classList.add("jump");
-  setTimeout(() => {
-    mario.classList.remove("jump");
-  }, 500);
+   mario.classList.add("jump");
+   setTimeout(() => {
+      mario.classList.remove("jump");
+   }, 500);
 };
 
+let score = 0;
+let contadorTempo = 0;
+const scoreNum = document.querySelector(".score-num");
+
 const loop = setInterval(() => {
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+   contadorTempo += 10;
 
-  const pipePosition = pipes.offsetLeft;
+   if (contadorTempo >= 100) {
+      score++;
+      scoreNum.textContent = score;
+      contadorTempo = 0;
+   }
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
-    pipes.style.animation = "none";
-    pipes.style.left = `${pipePosition}px`;
+   const cloudsPosition = +window.getComputedStyle(clouds).right.replace("px", "");
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+   const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-    mario.src = "./imgs/game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "50px";
-    clearInterval(loop);
-  }
+   const pipePosition = pipes.offsetLeft;
+   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
+      pipes.style.animation = "none";
+      pipes.style.left = `${pipePosition}px`;
+
+      mario.style.animation = "none";
+      mario.style.bottom = `${marioPosition}px`;
+
+      mario.src = "./imgs/game-over.png";
+      mario.style.width = "75px";
+      mario.style.marginLeft = "50px";
+
+      clouds.style.animation = "none";
+      clouds.style.right = `${cloudsPosition}px`;
+      clearInterval(loop);
+   }
 }, 10);
 
 document.addEventListener("keydown", jump);
