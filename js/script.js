6a507930522018ca3314ba9169bc4032d67523e9
MarianeMarinho <marianemarinho498@gmail.com>
2025-08-11 18:19:15 -0300

    feat: rename pipe class and adjust animation; update collision logic in script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const pipes = document.querySelector(".pipes");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -6,5 +7,26 @@ const jump = () => {
     mario.classList.remove("jump");
   }, 500);
 };
+
+const loop = setInterval(() => {
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  const pipePosition = pipes.offsetLeft;
+  //console.log(marioPosition);
+
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
+    pipes.style.animation = "none";
+    pipes.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./imgs/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+  }
+}, 10);
 
 document.addEventListener("keydown", jump);
